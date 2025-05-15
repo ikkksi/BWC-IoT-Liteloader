7f358773e9be7f3238011b5ce55ca9554e1a57ed
18501 <1850196117@qq.com>
2025-05-15 21:37:32 +0800

    add pluginsloader

--- src/main.py
+++ src/main.py
@@ -3,55 +3,24 @@ import json
 
 from libs import bot
 from libs.loger import aloger
+from libs.plm import plm
 
-
-p_ad = "ws://10.61.114.50:2048"
+p_ad = "ws://192.168.13.243:2048"
 my_app = bot.App(name="LiteLoader",auth_key="caixukun66",uri=p_ad)
 
 
-
-
 @my_app.register("message")
-async def handle(ws, send:callable, sender:str, type_:str, content:any, time:str):
-    """
-    :param ws: Websocket object
-    :param send: A fast function which you can use it to send data quickly
-    :param sender: sender name
-    :param type_:
-    :param content:
-    :param time:
-    :return: None
-    """
-    if sender == "cxk" and content == "connect success":
-        return_data = {"action": "serial_print", "param": {"port": 4, "rate": 115200, "data": f"page 1"}}
-        await asyncio.sleep(2) #等待开发板上电完成
-        await send(return_data)
+async def handler(ws, send:callable, sender:str, type_:str, content:any, time:str):
+    for call in plm.handle_list:
+        if call[1] != type_:
+            continue
+        await call[0](ws, send, sender, type_, content,time)
 
 
 
-@my_app.register("message")
-async def handle(ws, send:callable, sender:str, type_:str, content:any, time:str):
-    """
 
-    :param ws: Websocket object
-    :param send: A fast function which you can use it to send data quickly
-    :param sender: sender name
-    :param type_:
-    :param content:
-    :param time:
-    :return: None
-    """
-    if sender == "cxk":
-        try:
-            if content.get("event_name") == None:
-                return
-            else:
 
-                return_data = {"action": "serial_print", "param": {"port": 4, "rate": 115200, "data": f"page 1"}}
-                await asyncio.sleep(2) #等待开发板上电完成
-                await send(return_data)
-        except Exception as e:
-            pass
+
 
 
 
@@ -69,10 +38,7 @@ note = """
 
 
 if __name__ == "__main__":
-    print(note)
-    import plugins.agent as agent
-    import plugins.weather as we
 
-    aloger.info(f"""<{agent.INFO["plugin"]}{agent.version}> | {agent.INFO["author"]} | {agent.INFO["description"]}""")
-    aloger.info(f"""<{we.INFO["plugin"]}{we.version}> | {we.INFO["author"]} | {we.INFO["description"]}""")
+
+
     my_app.run()
--- src/plugins/plugins_demo/__init__.py
+++ src/plugins/plugins_demo/__init__.py
@@ -1,3 +1,6 @@
+import asyncio
+
+from libs.loger import aloger
 from libs.plm import plm
 
 
@@ -6,6 +9,48 @@ INFO = {
     "author":"Lixeer",
     "version":(1,0,0),
 }
-@plm.load(care_type="ss")
-def load_ss():
-    print("hello")
+
+
+@plm.load(care_type="message")
+async def handle(ws, send:callable, sender:str, type_:str, content:any, time:str):
+    """
+    :param ws: Websocket object
+    :param send: A fast function which you can use it to send data quickly
+    :param sender: sender name
+    :param type_:
+    :param content:
+    :param time:
+    :return: None
+    """
+    if sender == "cxk" and content == "connect success":
+        return_data = {"action": "serial_print", "param": {"port": 4, "rate": 115200, "data": f"page 1"}}
+        await asyncio.sleep(2) #等待开发板上电完成
+        await send(return_data)
+
+@plm.load("message")
+async def handle(ws, send:callable, sender:str, type_:str, content:any, time:str):
+    """
+
+    :param ws: Websocket object
+    :param send: A fast function which you can use it to send data quickly
+    :param sender: sender name
+    :param type_:
+    :param content:
+    :param time:
+    :return: None
+    """
+    if sender == "cxk":
+        try:
+            if content.get("event_name") == None:
+                return
+            else:
+
+                return_data = {"action": "serial_print", "param": {"port": 4, "rate": 115200, "data": f"page 1"}}
+                await asyncio.sleep(2) #等待开发板上电完成
+                await send(return_data)
+        except Exception as e:
+            pass
+
+@plm.load(care_type="message")
+async def handle(ws, send:callable, sender:str, type_:str, content:any, time:str):
+    aloger.info(f"{sender}")
